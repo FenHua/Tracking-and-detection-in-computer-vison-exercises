4d933e2366d81802deaaf57e8e8143454b8b2710
dan <warmspringwinds@gmail.com>
2014-10-22 12:30:45 +0400

    Functions descriptions were added

--- HM_1/pad_matrix.m
+++ HM_1/pad_matrix.m
@@ -1,6 +1,18 @@
-function [ result ] = padd_matrix( matrix, bottom_top_padding, side_padding, mode )
-%   PADD_MATRIX Summary of this function goes her
-%   Detailed explanation goes here
+function [ result ] = pad_matrix( matrix, bottom_top_padding, side_padding, mode )
+    %   PADD_MATRIX Creates a new matrix with padding on the borders.
+    %   It is used before applying a convolution or correlation to a
+    %   matrix. Has two options of padding: symmetric, where new values in
+    %   the padding area a symmetricly taken from the original matrix.
+    %   Replicate, where the values are taken only from the border of the
+    %   original matrix.
+    %   Input:
+    %   @matrix - original matrix to add padding to.
+    %   @bottom_top_padding - amount of new rows to add on the top and 
+    %   at the bottom.
+    %   @side_padding - amount of columns to add from the sides.
+    %   @mode - selected padding mode. Read description of the function.
+    %   Output:
+    %   @result - padded matrix. Size: (rows + bottom_top_padding*2, columns + side_padding) 
     
     [row_amount, column_amount] = size( matrix );
     
